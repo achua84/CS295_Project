ef90af22c367a7fb8f0a69ff989809c0e433b479
achua84 <andrea0502r@gmail.com>
2022-05-20 20:05:19 -0700

    Update helper.py

--- helper.py
+++ helper.py
@@ -5,7 +5,7 @@ feature_sizes = []
 P_matrices = []
 m = 10000 # Number of dimensions (can change)
 ID_length = 2000 # Number of ID images used for testing (can change)
-OOD_length = 1000 # Number of OOD images used for testing (can change)
+OOD_length = 1000 # Number of OOD images used for testing, x2 since there is near_OOD and far_OOD (can change)
 num_layers = 15 # Number of feature maps (max is 71, can change)
 
 
